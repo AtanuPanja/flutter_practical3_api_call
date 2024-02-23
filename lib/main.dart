53783e87737c1554b72248ce7ae7555416b5da1b
AtanuPanja <atanupanja9463@gmail.com>
2024-02-23 11:06:16 +0530

    Showed user data in list view
    Added ListOfData StatefulWidget
    Moved getData function inside ListOfData
    Updated state variable after setting
    state variable listOfData
    
    Used the state variable in ListOfData build method
    to populate the List using ListView.builder
    
    Showed the image, name, email and id of the users
    
    Added appBar with title Reqres users

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:dio/dio.dart';
 
 Dio dio = Dio();
 
-
 void main() {
   runApp(const MainApp());
 }
@@ -11,29 +10,92 @@ void main() {
 class MainApp extends StatelessWidget {
   const MainApp({super.key});
 
-  void getData(String url) async {
-    try {
-      final response = await dio.get(url);
-      print(response);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Reqres users'),
+        ),
+        body: ListOfData(),
+      ),
+    );
+  }
+}
 
-    } on DioException catch(e) {
+class ListOfData extends StatefulWidget {
+  const ListOfData({super.key});
+
+  @override
+  State<ListOfData> createState() => _ListOfDataState();
+}
+
+class _ListOfDataState extends State<ListOfData> {
+  late List<dynamic> listOfData;
+
+  void getData(String url) async {
+    Response response;
+
+    try {
+      response = await dio.get(url);
+      // print(response.data);
+      listOfData = response.data["data"];
+      print(listOfData);
+      setState(() {});
+    } on DioException catch (e) {
       print(e);
+      return;
     }
   }
 
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    listOfData = [{}];
+
+    getData('https://reqres.in/api/users?page=1');
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: ElevatedButton(
-            child: Text('Fetch data'),
-            onPressed: () {
-              getData('https://reqres.in/api/users?page=1');
-            },
+    return ListView.builder(
+      itemCount: listOfData.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: listOfData[index]["avatar"] != null
+              ? Image.network(
+                listOfData[index]["avatar"],
+              )
+              : Text("Image"),
+          trailing: Container(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+              borderRadius: BorderRadius.circular(15),
+            ),
+            padding: EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+            child: Text(
+              "${listOfData[index]["id"] ?? "ID"}",
+              style: TextStyle(
+                color: Colors.white,
+                fontSize: 14,
+              ),
+            ),
           ),
-        ),
-      ),
+          title: Text(
+            listOfData[index] != null
+                ? "${listOfData[index]['first_name']} ${listOfData[index]['last_name']}"
+                : "First_name Last_name",
+          ),
+          subtitle: Text(
+            listOfData[index] != null
+                ? "${listOfData[index]['email']}"
+                : "Email",
+          ),
+          contentPadding: EdgeInsets.all(12.0),
+        );
+      },
     );
   }
 }
